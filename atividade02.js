f93ef57b3855d3d7312685695e3582c9e49591dc
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2023-11-13 23:27:50 +0000

    laço faça enquanto - do while

---
+++ atividade02.js
@@ -0,0 +1,13 @@
+// Laço faça enquanto
+let i = 1
+do {
+    console.log("Botafogo Campeão")
+    i++
+}while (i<1);
+
+let contador = 0
+do{
+    console.log(contador)
+    contador++
+}while(contador<10)
+console.log("===>" + contador)
