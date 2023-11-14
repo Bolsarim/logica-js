135cec14ee3b6a2c297abea28c99da6567a60c4a
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2023-11-14 00:48:15 +0000

    laço de repetição com array

---
+++ atividade04.js
@@ -0,0 +1,29 @@
+/*let frutas = ["🍇", "🍓", "🍊" , "🥑"]
+
+for(let i = 0; i<4; i++){
+   console.log(frutas[i])
+    
+}
+
+
+
+let frutas = ["🍇", "🍓", "🍊" , "🥑"]
+frutas.push("🍐")
+
+for(let i = 0; i<frutas.length;i++){
+    console.log(frutas[i])
+}
+*/
+
+let frutas = ["🍇", "🍓", "🍊" , "🥑"]
+frutas.push("🍐")
+
+let i = 0
+while(i < frutas.length){
+    console.log(frutas[i])
+    i++
+}
+
+
+
+  
